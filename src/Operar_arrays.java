e2af441d288c59d46bdf1cde7bf361e06f4476cb
jmaes <jmaestrec9@gmail.com>
2020-02-10 09:58:58 +0100

    entornos3

--- src/Operar_arrays.java
+++ src/Operar_arrays.java
@@ -1,17 +1,24 @@
 
 public class Operar_arrays {
 	
-	private int array[];
+	private static int array[];
 
 	public static void main(String[] args) {
-		
+		rellenar_array();
+		imprimir_array();
 	}
 	
-	private void rellenar_array() {
+	private static void rellenar_array() {
 		array = new int[10];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) Math.round(Math.random()*20);
 		}
+	}
+	
+	private static void imprimir_array() {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		} 
 	}
 
 }
