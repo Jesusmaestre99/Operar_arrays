c80775804cb40d8ea979ba3a1ccf16a52b58c05a
jmaes <jmaestrec9@gmail.com>
2020-02-17 08:56:28 +0100

    cambioRama1

--- src/Operar_arrays.java
+++ src/Operar_arrays.java
@@ -1,24 +1,33 @@
-
+//Rama1
 public class Operar_arrays {
 	
-	private static int array[];
+	private static int ARRAY[];
 
 	public static void main(String[] args) {
 		rellenar_array();
 		imprimir_array();
+		sumar_array();
 	}
 	
 	private static void rellenar_array() {
-		array = new int[10];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = (int) Math.round(Math.random()*20);
+		ARRAY = new int[10];
+		for (int i = 0; i < ARRAY.length; i++) {
+			ARRAY[i] = (int) Math.round(Math.random()*20);
 		}
 	}
 	
 	private static void imprimir_array() {
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		for (int i = 0; i < ARRAY.length; i++) {
+			System.out.println(ARRAY[i]);
 		} 
+	}
+	
+	private static void sumar_array() {
+		int suma = 0;
+		for (int i = 0; i < ARRAY.length; i++) {
+			suma += ARRAY[i];
+		}
+		System.out.println(suma);
 	}
 
 }
